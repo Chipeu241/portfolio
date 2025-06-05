e522de5400682f37983c03e2285447a8fd6a3c3b
Quanh <quynhanhnguyen2005.nb@gmail.com>
2025-06-05 16:24:02 +0700

    Fix web

--- fintech/views.py
+++ fintech/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 def index(request):
     return render(request, 'fintech/index.html')
