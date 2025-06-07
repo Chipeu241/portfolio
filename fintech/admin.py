772e53868a590e9d5735fdeb8ac27cea72a3b649
Quanh <quynhanhnguyen2005.nb@gmail.com>
2025-06-07 21:23:11 +0700

    sửa bcao

--- fintech/admin.py
+++ fintech/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import taiKhoan, Post, Danhmuc
+from django.urls import reverse
+from django.utils.html import format_html
+from django.shortcuts import redirect
+from django.utils.translation import gettext_lazy as _
 # Register your models here.
 admin.site.register(Post)
 class AdminPost(admin.ModelAdmin):
@@ -20,3 +24,27 @@ class TaiKhoanAdmin(admin.ModelAdmin):
         qs = super().get_queryset(request)
         return qs.filter(is_staff=False)  # chỉ user thường
 admin.site.register(taiKhoan, TaiKhoanAdmin)
+
+class DummyReportModel:
+    class _meta:
+        app_label = 'fintech'
+        model_name = 'baocaothongke'
+        verbose_name = 'Báo cáo thống kê'
+        verbose_name_plural = 'Báo cáo thống kê'
+
+    def __str__(self):
+        return "Báo cáo thống kê"
+
+class ReportAdmin(admin.ModelAdmin):
+    def changelist_view(self, request, extra_context=None):
+        return redirect(reverse('admin_baocao'))
+
+    def has_add_permission(self, request):
+        return False
+    def has_change_permission(self, request, obj=None):
+        return False
+    def has_delete_permission(self, request, obj=None):
+        return False
+    def has_view_permission(self, request, obj=None):
+        return True
+
