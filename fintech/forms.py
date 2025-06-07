ac7fa75c1a8adbd2700b6fd262386dc35d6c163d
Quanh <quynhanhnguyen2005.nb@gmail.com>
2025-06-07 14:35:11 +0700

    fix taiKhoan

--- fintech/forms.py
+++ fintech/forms.py
@@ -1,16 +1,12 @@
 from django import forms
 from .models import Comment
-from .models import taiKhoan
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content', 'name', 'email']
-class CreateUserForm(forms.ModelForm):
+class CreateUserForm(UserCreationForm):
     class Meta:
-        model = taiKhoan
-        fields = ['tenTaiKhoan', 'matKhau', 'hoTen', 'email', 'soDienThoai', 'diaChi']
-        widgets = {
-            'matKhau': forms.PasswordInput(),
-        }
+        model = User  
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
